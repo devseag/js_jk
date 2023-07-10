a1c7947cbd896923d9221064bfed5f950bb31b91
devseag <example@gmail.com>
2023-07-10 13:26:07 -0300

    add callback function

---
+++ js/script_section_2_30_callback_function.js
@@ -0,0 +1,69 @@
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('How many films you already watched?','');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('How many films you already watched?','');
+	}
+}
+
+start();
+
+console.log(numberOfFilms);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+function rememberMyFilms () {
+	for ( let i =0; i < 2; i++) {
+		const a = prompt('Tell me the last movie you have watched','').trim(),
+			  b = prompt('How good was that?','');
+	
+		if (a != null && b != null && a != '' && b != '' && a.length < 50){
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if( personalMovieDB.count < 10) {
+		console.log('Just a few movies');
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log('Too classic');
+	} else if (personalMovieDB.count >= 30) {
+		console.log('Nice!');
+	} else {
+		console.log('Error');
+	}
+}
+
+detectPersonalLevel();
+
+function showMyDb (hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDb(personalMovieDB.privat);
+
+function writeYourGenres () {
+	for ( let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] =  prompt(`Your fav genre with number ${i}`);
+	}
+}
+
+writeYourGenres();
