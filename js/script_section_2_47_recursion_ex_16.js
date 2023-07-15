e67811fd7d8e003d4b7ef49040d88a0bbad15928
devseag <example@gmail.com>
2023-07-15 12:25:52 -0300

    add recursion exer 16

---
+++ js/script_section_2_47_recursion_ex_16.js
@@ -0,0 +1,128 @@
+// // function pow(x, n) {
+// //     let result = 1;
+
+// //     for ( let i = 0; i < n; i++){
+// //         result *= x;
+// //     }
+
+// //     return result;
+// // }
+
+// function pow(x, n) {
+//     if ( n === 1) {
+// 		return x;
+// 	} else {
+// 		return x * pow(x, n - 1 );
+// 	}
+// }
+
+
+// pow(2, 1);
+// pow(2, 2);
+// pow(2, 3);
+// pow(2, 4);
+
+let students = {
+	js: [{
+		name: 'John',
+		progress: 100
+	}, {
+		name: 'Ivan',
+		progress: 60
+	}],
+
+	html: {
+		basic: [{
+			name: 'Peter',
+			progress: 20
+		}, {
+			name: 'Ann',
+			progress: 18
+		}],
+
+		pro: [{
+			name: 'Sam',
+			progress: 10
+		}],
+
+		semi: {
+			students: [{
+				name: 'Test',
+				progress: 100
+			}]
+		}
+	}
+};
+
+function getTotalPorgressByIteration(data) {
+	let total = 0;
+	let students = 0;
+
+	for (let course of Object.values(data)) {
+		if (Array.isArray(course)) {
+			students += course.length;
+
+			for (let i = 0; i < course.length; i++) {
+				total += course[i].progress;
+			}
+		} else {
+			for (let subCourse of Object.values(course)) {
+				students += subCourse.length;
+
+				for (let i = 0; i < subCourse.length; i++) {
+					total += subCourse[i].progress;
+				}
+			}
+		}
+	}
+
+	return total / students;
+}
+
+
+// console.log(getTotalPorgressByIteration(students));
+
+
+
+function getTotalPorgressByRecursion(data) {
+	if (Array.isArray(data)) {
+		let total = 0;
+
+		for (let i = 0; i < data.length; i++) {
+			total += data[i].progress;
+		}
+
+		return [total, data.length];
+	} else {
+		let total = [0, 0];
+
+		for (let subData of Object.values(data)) {
+			const subDataArr = getTotalPorgressByRecursion(subData);
+			total[0] += subDataArr[0];
+			total[1] += subDataArr[1];
+		}
+
+		return total;
+	}
+}
+
+const result = getTotalPorgressByRecursion(students);
+
+console.log(result[0]/result[1]);
+
+
+
+
+function factorial(num) {
+    if ( typeof(num) !== 'number' || !Number.isInteger(num)){
+		return 'Error';
+	}
+	
+	if ( num < 1 ){
+		return 1;
+	} else {
+		return num *  factorial(num - 1);
+	}
+}
+
+console.log(factorial(4.2));
