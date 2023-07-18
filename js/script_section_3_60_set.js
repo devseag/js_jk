792b429321ddadbcaef5d1004cbea94916761004
devseag <example@gmail.com>
2023-07-18 15:41:15 -0300

    add set

---
+++ js/script_section_3_60_set.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+// const arr = [1, 1, 2, 2, 4, 5, 6, 5];
+const arr = ['Alex', 'Ann', 'Oleg', 'Alex'];
+
+function unique(arr) {
+	return Array.from(new Set(arr));
+}
+
+console.log(unique(arr));
+
+// const set = new Set(arr);
+
+// set.add('Ivan');
+// set.add('Oleg');
+
+// set.add('Ivan')
+// 	.add('Oleg');
+
+
+// console.log(set);
+
+// set.delete(value);
+// set.has(value);
+// set.clear();
+// set.size;
+
+// for (let value of set) console.log(value);
+// set.forEach( (value, valueAgain, set) => {
+// 	console.log(value, valueAgain);
+// });
+
+// console.log(set.values());
+// console.log(set.keys()); // the same as values
+// console.log(set.entries());
+
+// function unique(arr) {
+// 	return new Set(arr);
+// }
+
