cff39823d1938537f1736ff27911426d7375358f
devseag <example@gmail.com>
2023-07-19 19:05:19 -0300

    add WeakMap WeakSet

---
+++ js/script_section_4_67_weakMap_weakSet.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// let user = {name: 'Ivan'};
+
+// // const arr = [user];
+// // let map = new Map();
+// let map = new WeakMap();
+// map.set(user, 'data');
+
+// user = null;
+
+// // console.log(user);
+// // console.log(arr[0]);
+// // console.log(map.keys());
+// console.log(map.has(user)); // false
+// console.log(map); // false
+
+let cache = new WeakMap();
+
+function cacheUser(user) {
+    if (!cache.has(user)) {
+        cache.set(user, Date.now());
+    }
+    return cache.get(user);
+}
+
+let lena = {name: 'Elena'};
+let alex = {name: 'Alex'};
+
+cacheUser(lena);
+cacheUser(alex);
+
+lena = null;
+
+console.log(cache.has(lena));
+console.log(cache.has(alex));
+
+// WeakSet
+// add, has, delete
+
+let messages = [
+    {text: 'Hello', from: 'John'},
+    {text: 'World', from: 'Alex'},
+    {text: '...', from: 'M'}
+];
+
+let readMessages = new WeakSet();
+
+readMessages.add(messages[0]);
+// readMessages.add(messages[1]);
+
+readMessages.add(messages[0]);
+messages.shift();
+console.log(readMessages.has(messages[0]));
+
+
