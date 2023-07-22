809ff7f403af5a6c7d92798e7755ec4d9a5b08d8
devseag <example@gmail.com>
2023-07-22 14:22:16 -0300

    add promises

---
+++ js/script_section_5_86_promises.js
@@ -0,0 +1,91 @@
+'use strict';
+
+// console.log('Data request...');
+
+// // const req = new Promise(function(resolve, reject){
+// const req = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		console.log('Data preparing...');
+	
+// 		const product = {
+// 			name: 'TV',
+// 			price: 2000
+// 		};
+	
+// 		resolve(product);
+// 	}, 2000);
+// });
+
+// // req.then((product) => {
+// // 	// console.log('Data is ready');
+// // 	setTimeout(() => {
+// // 		product.status = 'order';
+// // 		console.log(product);
+// // 	}, 2000);
+// // });
+
+// // req.then((product) => {
+// // 	const req2 = new Promise((resolve, reject) => {
+// // 		setTimeout(() => {
+// // 			product.status = 'order';
+// // 			resolve(product);
+// // 		}, 2000);
+// // 	});
+
+// // 	req2.then(data => {
+// // 		console.log(data);
+// // 	});
+// // });
+
+// req.then((product) => {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			product.status = 'order';
+// 			resolve(product);
+// 			// reject();
+// 		}, 2000);
+// 	}).then(data => {
+// 		data.modify = true;
+// 		return data;
+// 	}).then((data) => {
+// 		console.log(data);
+// 	});
+// }).catch(() =>{
+// 	console.error('Error!!!');
+// }).finally(() => {
+// 	console.log('Finally');
+// });
+
+
+
+
+// setTimeout(() => {
+// 	console.log('Data preparing...');
+
+// 	const product = {
+// 		name: 'TV',
+// 		price: 2000
+// 	};
+
+// 	setTimeout(() => {
+// 		product.status = 'order';
+// 		console.log(product);
+// 	}, 2000);
+// }, 2000);
+
+const test = time => {
+	return new Promise(resolve => {
+		setTimeout(() => resolve(), time);
+	});
+};
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+// 	console.log('All');
+// });
+
+Promise.race([test(1000), test(5000)]).then(() => {
+	console.log('All');
+});
