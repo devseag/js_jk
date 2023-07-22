a696c896b430c605855711ba6f730eb78b59f673
devseag <example@gmail.com>
2023-07-21 22:18:44 -0300

    add JSON clone objects

---
+++ js/script_section_5_82_JSON_clone_objects.js
@@ -0,0 +1,17 @@
+'use strict';
+     
+const persone = {
+	name: 'Alex',
+	tel: '+744444',
+	parents: {
+		mom: 'Olga',
+		dad: 'Mike'
+	}
+};
+
+// console.log(JSON.stringify(persone));
+// console.log(JSON.parse(JSON.stringify(persone)));
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
