ac1485f2517e8fca6acd000f4f1978f11432f504
devseag <example@gmail.com>
2023-07-27 13:54:45 -0300

    add new promise ex

--- js/script_section_5_86_promises.js
+++ js/script_section_5_86_promises.js
@@ -89,3 +89,29 @@ const test = time => {
 Promise.race([test(1000), test(5000)]).then(() => {
 	console.log('All');
 });
+
+
+
+
+const p1 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 1000, 'one');
+});
+const p2 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 2000, 'two');
+});
+const p3 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 3000, 'three');
+});
+const p4 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 4000, 'four');
+});
+const p5 = new Promise((resolve, reject) => {
+	reject('reject');
+});
+   
+Promise.all([p1, p2, p3, p4, p5]).then(value => {
+	console.log(value);
+}, reason => {
+	console.log(reason); // reject
+}
+);
